74d1d23c96e267dcdec121ffc805db4b76861b4a
TifaniBygati <dbrnjh556@gmail.com>
2026-05-22 15:54:46 +0300

    Add in-memory clients endpoint

---
+++ app/local_base.py
@@ -0,0 +1,20 @@
+data = [
+    {
+        "id": 1,
+        "order_id": "random-order-123",
+        "street": "Пушкина",
+        "house": "7",
+        "apartment": "17",
+        "phone": "+79990000000",
+        "comment": "домофон не работает"
+    },
+    {
+        "id": 2,
+        "order_id": "random-order-456",
+        "street": "Ленина",
+        "house": "10",
+        "apartment": "25",
+        "phone": "+79990000001",
+        "comment": "оставить у двери"
+    }
+]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.local_base import data
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.get("/clients")
+def get_all_clients():
+    return data
