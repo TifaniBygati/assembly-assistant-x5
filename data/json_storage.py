dc205c36abd3fa1172448d24c467338302bb7f48
TifaniBygati <dbrnjh556@gmail.com>
2026-06-01 13:10:09 +0300

    Add sqlite migration script

--- data/json_storage.py
+++ data/json_storage.py
@@ -1,7 +1,7 @@
 import json
 
 def load_data():
-    with open('./data/database.json', 'r', encoding='utf-8') as file:
+    with open('../data/database.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
 
---
+++ scripts/migrate_json_to_sqlite.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+from data.json_storage import load_data
+
+DB_PATH = "../data/clients.db"
+
+data = load_data()
+
+db = sqlite3.connect(DB_PATH)
+cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS clients (
+        id INTEGER PRIMARY KEY,
+        order_id TEXT,
+        street TEXT,
+        house TEXT,
+        apartment TEXT,
+        phone TEXT,
+        comment TEXT
+    )
+    """)
+cursor.execute('DELETE FROM clients')
+
+for client in data:
+    cursor.execute("INSERT INTO clients(id, order_id, street, house, apartment, phone, comment) VALUES (?,?,?,?,?,?,?)",
+                   (client['id'],
+                    client['order_id'],
+                    client['street'],
+                    client['house'],
+                    client['apartment'],
+                    client['phone'],
+                    client['comment']
+                    ))
+db.commit()
+db.close()
