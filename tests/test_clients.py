4006898ff3f1855971248177ace44ce2d0a485e4
TifaniBygati <dbrnjh556@gmail.com>
2026-06-20 12:30:33 +0300

    Add client search API test

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -28,3 +28,39 @@ def test_get_client_by_id():
     assert response.status_code == 200
     assert isinstance(body, list)
     assert body[0]['client_id'] == client_id
+
+def test_get_client_by_address():
+
+    response = client.get('/clients')
+
+    assert response.status_code == 200
+
+    clients = response.json()
+
+    assert clients != []
+
+    client_id = clients[0]['client_id']
+
+    response = client.get(f'/clients/{client_id}')
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert isinstance(body, list)
+    assert body != []
+    assert body[0]['client_id'] == client_id
+
+    street = body[0]['street']
+    house = body[0]['house']
+
+    response = client.get('/clients/search', params={'street': street, 'house': house})
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert isinstance(body, list)
+    assert body != []
+    assert body[0]['street'] == street
+    assert body[0]['house'] == house
